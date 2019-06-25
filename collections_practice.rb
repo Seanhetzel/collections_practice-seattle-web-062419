f5d3e6afbc919816464d4d627b683ee851b17f41
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-25 20:48:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  new_array = ["i"]
+  new_array = []
   array.each do |word|
     if word.length > new_array.last.length
       new_array << word
