342201c98677fa35797b0a3f63a746d1c17e1296
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-25 20:47:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,13 @@ end
 def sort_array_desc(array)
   array.sort.reverse
 end
+
+def sort_array_char_count(array)
+  new_array = []
+  array.each do |word|
+    if word.length > new_array.last.length
+      new_array << word
+    end
+  end
+  new_array
+end
